1b706983835c7081d343b376c88a70c70e4e1616
Nishant <iamb.nishant@gmail.com>
2024-12-14 12:40:54 +1000

    Major CRUD Functionality Changes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
     return (
       <footer className="bg-gray-200 py-4 text-center">
-        <p>&copy; {currentYear} Callum Bir. All rights reserved.</p>
+        <p>&copy; {currentYear} Nishant Bhattarai. All rights reserved.</p>
       </footer>
     )
   }
--- prisma/migrations/20241214023115_init/migration.sql
+++ prisma/migrations/20241214023115_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "email" TEXT NOT NULL,
+    "email" TEXT,
     "phoneNumber" TEXT NOT NULL,
     "location" TEXT,
 
